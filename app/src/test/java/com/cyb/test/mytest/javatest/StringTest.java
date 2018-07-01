a3a9b967e725cead009d8f75960e2ecd18641d95
yongbing.chao <yongbing.chao@husor.com>
2018-07-01 13:34:58 +0800

    java知识点

---
+++ app/src/test/java/com/cyb/test/mytest/javatest/StringTest.java
@@ -0,0 +1,13 @@
+package com.cyb.test.mytest.javatest;
+
+/**
+ * Created by yongbing.chao on 2018/6/11.
+ */
+
+public class StringTest {
+    public void test() {
+        String a = "a";
+        String myStr = "aa" + "bb" + a + "cc" + "dd";
+        System.out.println("My String:" + myStr);
+    }
+}
