b164956926390d7c2172f70133365dde379eddb7
晁永兵 <yongbingchao@gmail.com>
2020-05-21 22:31:08 +0800

    eventbus butterknife

--- app/src/main/java/com/cyb/test/mytest/view/RecyclerViewActivity.java
+++ app/src/main/java/com/cyb/test/mytest/view/RecyclerViewActivity.java
@@ -1,30 +1,55 @@
 package com.cyb.test.mytest.view;
 
+import android.content.Intent;
+import android.os.Looper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
+import android.util.EventLog;
+import android.util.Printer;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.cyb.test.mytest.MyEventBusIndex;
 import com.cyb.test.mytest.R;
+import com.cyb.test.mytest.anim.SimpleActivity;
+
+import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
+import org.greenrobot.eventbus.ThreadMode;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 import static android.support.v7.widget.RecyclerView.VERTICAL;
 
 public class RecyclerViewActivity extends AppCompatActivity {
 
-    private RecyclerView recyclerView;
+//    private RecyclerView recyclerView;
     private RecyclerView.Adapter adapter;
+
+    @BindView(R.id.recyclerView)
+    public RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recycler_view);
-        recyclerView = findViewById(R.id.recyclerView);
+        ButterKnife.bind(this);
+//        recyclerView = findViewById(R.id.recyclerView);
         recyclerView.setLayoutManager(new StaggeredGridLayoutManager(3, VERTICAL));
 //        recyclerView.setLayoutManager(new LinearLayoutManager(this, VERTICAL, false));
+        Looper.getMainLooper().setMessageLogging(new Printer() {
+            @Override
+            public void println(String x) {
+
+            }
+        });
+
         adapter = new RecyclerView.Adapter() {
             @Override
             public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
@@ -36,6 +61,12 @@ public class RecyclerViewActivity extends AppCompatActivity {
             public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
                 ((TextView) holder.itemView).setText("rv item： " + position);
                 ((TextView) holder.itemView).setHeight(200);
+                ((TextView) holder.itemView).setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        startActivity(new Intent(RecyclerViewActivity.this, SimpleActivity.class));
+                    }
+                });
             }
 
             @Override
@@ -52,11 +83,37 @@ public class RecyclerViewActivity extends AppCompatActivity {
         recyclerView.setAdapter(adapter);
 
         adapter.notifyDataSetChanged();
+
+        EventBus.builder().addIndex(new MyEventBusIndex()).installDefaultEventBus();
+
+        EventBus eventBus = EventBus.getDefault();
+        eventBus.register(this);
+        eventBus.post(new EventObj("124"));
     }
 
     static class MyVH extends RecyclerView.ViewHolder {
         public MyVH(TextView itemView) {
             super(itemView);
+        }
+    }
+
+    @Subscribe(threadMode = ThreadMode.MAIN, priority = 100)
+    public void onEvent(EventObj obj) {
+        Toast.makeText(this, obj.text, Toast.LENGTH_LONG).show();
+    }
+
+
+    @Subscribe(threadMode = ThreadMode.MAIN, priority = 100)
+    public void onEvent22(EventObj obj) {
+        Toast.makeText(this, obj.text, Toast.LENGTH_LONG).show();
+    }
+
+
+    static class EventObj {
+        String text = "";
+
+        public EventObj(String text) {
+            this.text = text;
         }
     }
 }
